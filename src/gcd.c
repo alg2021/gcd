1645978fbcf492bf08df0fbdf9a17f821e931ceb
Ryuhei Mori <mori@c.titech.ac.jp>
2019-08-29 13:06:11 +0900

    initial

---
+++ src/gcd.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+const int n = 10000;
+
+unsigned int Euclidean_gcd_rec(unsigned int x, unsigned int y){
+}
+
+unsigned int Euclidean_gcd_itr(unsigned int x, unsigned int y){
+}
+
+unsigned int binary_gcd_rec(unsigned int x, unsigned int y){
+}
+
+unsigned int binary_gcd_itr(unsigned int x, unsigned int y){
+}
+
+
+int main(){
+  unsigned int i, j, c;
+
+
+  c = 0;
+  for(i = 1; i <= n; i++){
+    for(j = i+1; j <= n; j++){
+      if(Euclidean_gcd_rec(i, j) == 1){
+        c++;
+      }
+    }
+  }
+  c = 2*c+1;
+
+  printf("%f\n", (double) c / (n*n));
+  return 0;
+}
