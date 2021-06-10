43d18545c6e7d67f491f7b8c623f641cab8eb487
mizuki takusagawa <takusagawa.m.aa@m.titech.ac.jp>
2021-06-10 23:26:42 +0900

    test2

--- src/gcd.c
+++ src/gcd.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-tsts
+
 const int n = 1000;
 
 unsigned int Euclidean_gcd_rec(unsigned int x, unsigned int y){
+	if (x == 0) return y;
+	return Euclidean_gcd_rec(y % x, x);
 }
 
 /*
