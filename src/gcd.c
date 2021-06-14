4916630e024356470e4bf4667c62ebd3e45f17c5
ryu769 <79493725+ryu769@users.noreply.github.com>
2021-06-14 23:56:33 +0900

    Update gcd.c

--- src/gcd.c
+++ src/gcd.c
@@ -3,12 +3,21 @@
 const int n = 1000;
 
 unsigned int Euclidean_gcd_rec(unsigned int x, unsigned int y){
+  if (x == 0) return y;
+  return Euclidean_gcd_rec(y % x, x);
+}
+
+
+unsigned int Euclidean_gcd_itr(unsigned int x, unsigned int y){
+  while (x != 0) {
+    unsigned int z = x;
+    x = y % x;
+    y = z;
+  }
+  return y;
 }
 
 /*
-unsigned int Euclidean_gcd_itr(unsigned int x, unsigned int y){
-}
-
 unsigned int binary_gcd_rec(unsigned int x, unsigned int y){
 }
 
