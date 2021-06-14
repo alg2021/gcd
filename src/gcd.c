68ac656941da5d0be99b1d070b5b1307d5c0d56d
糟谷颯希 <kasuyasatsuki@satsuki-mbp.local>
2021-06-14 18:35:41 +0900

    revised

--- src/gcd.c
+++ src/gcd.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
-const int n = 1000;
+const int n = 10000;
 
 unsigned int Euclidean_gcd_rec(unsigned int x, unsigned int y){
+    if(x == 0) return y;
+    return Euclidean_gcd_rec(y % x, x);
 }
 
-/*
+
 unsigned int Euclidean_gcd_itr(unsigned int x, unsigned int y){
+    while(x != 0){
+    unsigned int z = x; x = y % x;
+    y = z;
+    }
+    return y;
 }
-
+/*
 unsigned int binary_gcd_rec(unsigned int x, unsigned int y){
 }
 
