fdc562afc76dcf79b02950b86eaade25a8065768
Tomoaki Kawasumi <kawasemitomo@outlook.jp>
2021-06-18 00:54:06 +0900

    revised

--- src/gcd.c
+++ src/gcd.c
@@ -2,11 +2,19 @@
 
 const int n = 1000;
 
-unsigned int Euclidean_gcd_rec(unsigned int x, unsigned int y){
+unsigned int Euclidean_gcd_rec(unsigned int x, unsigned int y) {
+  if (x == 0) return y;
+  return Euclidean_gcd_rec(y % x, x);
 }
 
 /*
 unsigned int Euclidean_gcd_itr(unsigned int x, unsigned int y){
+  while (x != 0){
+    unsigned int z = x;
+    x = y % x;
+    y = z;
+  }
+  return y;
 }
 
 unsigned int binary_gcd_rec(unsigned int x, unsigned int y){
@@ -18,19 +26,16 @@ unsigned int binary_gcd_itr(unsigned int x, unsigned int y){
 
 
 int main(){
-  unsigned int i, j, c;
+  unsigned int c = 0;
 
-
-  c = 0;
-  for(i = 1; i <= n; i++){
-    for(j = i + 1; j <= n; j++){
-      if(Euclidean_gcd_rec(i, j) == 1){
+  for(int i = 1; i <= n; i++){
+    for(int j = i + 1; j <= n; j++){
+      if (Euclidean_gcd_rec(i, j) == 1){
         c++;
       }
     }
   }
   c = 2 * c + 1;
-
-  printf("%f\n", (double) c / (n * n));
+  printf("%f\n", ((double) c) / (n * n));
   return 0;
 }
